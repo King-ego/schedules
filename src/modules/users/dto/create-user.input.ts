255bec3121f423f4b34fb57bbabaaf3a156fc6a1
diego <diegosantos4957@gmail.com>
2025-03-15 11:50:34 -0300

    feat: remove comments

--- src/modules/users/dto/create-user.input.ts
+++ src/modules/users/dto/create-user.input.ts
@@ -5,9 +5,8 @@ import { Type } from "class-transformer";
 @InputType()
 export class CreateUserInput {
   @Field()
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   @IsString()
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   @Type(() => String)
   name: string;
+
 }
